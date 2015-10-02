c30b5845f700c9d3025fd692199807844d8b49c3
Henry Schaumburger <per4mnce@gmail.com>
2015-10-02 21:24:57 +0000

    Added Welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
+  #get 'welcome/index'
+  #get 'welcome/about'
 
   devise_for :users
-  
-  #get 'about' => 'welcome#about'
   get 'welcome' => 'welcome#index'
   root to: 'welcome#index'
 end
