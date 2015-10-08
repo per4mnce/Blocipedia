9c7868896b0b63e6db681691fbc3ecf56e1969b7
Henry Schaumburger <per4mnce@gmail.com>
2015-10-08 15:32:21 +0000

    Upgrade and downgrade account

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   
   resources :charges, only: [:new, :create]
   
+  get 'downgrade' => 'charges#downgrade'
+  
   get 'welcome' => 'welcome#index'
   
   root to: 'welcome#index'
